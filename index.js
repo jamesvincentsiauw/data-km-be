569841fb855a5999ef4a7359f13af9828a05e9dd
Vincent <jamesvincentsiauw@gmail.com>
2020-08-16 18:15:03 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable linebreak-style */
+const {ApolloServer} = require('apollo-server');
+const {loadFile} = require('graphql-import-files');
+const resolver = require('./schema/resolver');
+
+const server = new ApolloServer({
+  typeDefs: loadFile('./schema/typedefs.gql'),
+  resolvers: resolver,
+});
+
+server.listen().then(({url}) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ schema/resolver.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+const resolver = {
+  Query: {
+    getInfographics: () => {
+      /* Function Goes Here */
+    },
+  },
+  Mutation: {
+    addInfographic: (parent, args) => {
+      /* Function Goes Here */
+    },
+  },
+};
+
+module.exports = resolver;
